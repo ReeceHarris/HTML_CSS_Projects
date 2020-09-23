73b5072fa784f5281f01af974d3db547a5a34c17
Reece <ReeceHarris423@gmail.com>
2020-09-23 17:06:58 -0600

    testing

---
+++ Basic_HTML_and_JavaScript_2/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+alert("Hello, World! I am an alert box!");
+ 
+
+
+function My_First_Fuction () {
+
+   var String = "Kiss me, I'm Irish";
+   var result = String.fontcolor("green");
+   document.getElementById("Irish").innerHTML = result ; 
+}
